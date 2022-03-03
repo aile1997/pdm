9e1e2b0eb2aa242f2794b455b839e4e0b2dbcbe4
牛思顿 <1622598684@qq.com>
2022-03-03 17:21:19 +0800

    x

---
+++ precache-manifest.5d36d70ccbc44425ccbb4892508c1bc6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "218d3ea9ed80dcff98de",
+    "url": "/pdm/static/js/main.fcee81d9.chunk.js"
+  },
+  {
+    "revision": "6d84a2991e23601386a0",
+    "url": "/pdm/static/js/runtime~main.004c0b68.js"
+  },
+  {
+    "revision": "d369c0ef100e1124d0a6",
+    "url": "/pdm/static/js/2.072d329d.chunk.js"
+  },
+  {
+    "revision": "299ebe7b5025e03d361634963a77ba70",
+    "url": "/pdm/index.html"
+  }
+];
